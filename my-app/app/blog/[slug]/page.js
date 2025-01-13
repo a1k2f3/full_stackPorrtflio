fa28ea12b5dc1d06d715f380338f58ecd0b86043
akif <su92-bssem-f22-118@superior.edu.pk>
2025-01-13 21:16:22 +0500

    git
    iji

--- my-app/app/blog/[slug]/page.js
+++ my-app/app/blog/[slug]/page.js
@@ -6,7 +6,6 @@ export default function Post() {
   const { slug } = useParams();
   const post = blogPosts.find((p) => p.slug === slug);
   if (!post) return <p className="text-center mt-10">Post not found</p>;
-
   return (
     <div className="max-w-3xl mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
