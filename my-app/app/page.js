b204cda466bfac1ddb98173f4cd05d298d67553c
akif <su92-bssem-f22-118@superior.edu.pk>
2025-06-30 13:44:12 +0500

    add whatsapp

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -137,6 +137,18 @@ export default function Home() {
       width={320}
       height={320}
     />
+      <a
+      href="https://wa.me/+923117687149" // Replace with your number
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-5 right-5 z-50"
+    >
+      <img
+        src="https://img.icons8.com/color/48/000000/whatsapp.png"
+        alt="Chat on WhatsApp"
+        className="w-12 h-12"
+      />
+    </a>
   </div>
         
       </motion.section>
